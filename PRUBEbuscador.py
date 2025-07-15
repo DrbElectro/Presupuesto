ae0a6848b741b8d7021d112cb3e4f6d4d43e5d64
DrbElectro <drb.electro@gmail.com>
2025-07-15 09:16:08 -0300

    Update PRUBEbuscador.py

--- PRUBEbuscador.py
+++ PRUBEbuscador.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 from datetime import date
 
-EXCEL_PATH = r'G:\Mi unidad\Proveedores\Final\Proveedores.xlsx'
+EXCEL_PATH = Proveedores.xlsx'
 
 def solapa_presupuesto(precios_df, costos_df, clave_estado="presupuesto_items", titulo="Presupuesto"):
     st.subheader(titulo)
